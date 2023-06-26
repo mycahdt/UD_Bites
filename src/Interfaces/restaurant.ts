096785b604d64e1214f5d3e9d35bce926be13de7
mycahdt <mycahdt@udel.edu>
2023-06-25 20:32:26 -0400

    restaurant data

---
+++ src/Interfaces/restaurant.ts
@@ -0,0 +1,12 @@
+export interface Restaurant {
+    name: string;
+    location: string;
+    price: string;
+    sitdown: boolean;
+    bar: boolean;
+    boba: boolean;
+    coffee: boolean;
+    pizza: boolean;
+    dessert: boolean;
+    asian: boolean;
+}
